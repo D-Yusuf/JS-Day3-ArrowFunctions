80d317128ff6ca1ccbe9723770380c91e9e1467c
Yusuf Alnasiri <alnasiriyusuf@gmail.com>
2024-08-06 18:25:34 +0300

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+const greet = (name)=>{
+    return `Hello, ${name}`
+}
+const sum = (n1, n2)=>{
+    return n1+n2
+}
+const square = (n)=>{
+    return n*n
+}
+const squareArr = (numArr)=>{
+    return numArr.map(num=>{
+        return num*num
+    })
+}
